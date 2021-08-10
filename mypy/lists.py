e5503d8505f476fa23c3c1035e325e30c36f6c78
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-08-10 10:49:45 -0400

    add remove dupes

--- mypy/lists.py
+++ mypy/lists.py
@@ -21,3 +21,16 @@ def write_file_lists(filename: str, lists: Iterable[Iterable], list_sep: str = "
                     f.write(list_sep)
                 f.write(i)
             f.write("\n")
+
+
+def remove_duplicates_in_place(l: list) -> list:
+    """Still uses sets, but preserves the order of the remaining elements, which turning into a set and then back would not."""
+    unique = set()
+    i = 0
+    while i < len(l):
+        if l[i] in unique:
+            del l[i]
+        else:
+            unique.add(l[i])
+            i += 1
+    return l
