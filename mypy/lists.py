605482484e649db8dc1da326b5a0e25fe26d125f
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-08-15 15:47:55 -0400

    refactors

--- mypy/lists.py
+++ mypy/lists.py
@@ -1,16 +1,17 @@
-from typing import Generator, Iterable
+import typing
+import re
 
 
-def read_file_lists(filename: str, list_sep: str = ",", comment: str = "#") -> Generator:
+def read_file_lists(filename: str, list_sep: str = "\s*,\s*", comment: str = "\s*#") -> typing.Generator:
     with open(filename) as file:
         for line in file:
             line = line.strip()
-            if line == "" or line[:len(comment)] == comment:
+            if line.isspace() or re.match(comment, line):
                 continue
             yield line.split(list_sep)
 
 
-def write_file_lists(filename: str, lists: Iterable[Iterable], list_sep: str = ",") -> None:
+def write_file_lists(filename: str, lists: typing.Iterable[typing.Iterable], list_sep: str = ",") -> None:
     with open(filename, "w", encoding="UTF-8") as f:
         for l in lists:
             first = True
