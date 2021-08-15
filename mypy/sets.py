fbe1e00e6f7f6e51cd3296d2a2716f99129ba816
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-08-15 15:48:03 -0400

    support more python operations

--- mypy/sets.py
+++ mypy/sets.py
@@ -2,10 +2,13 @@ from typing import Hashable
 
 
 class SetGroup:
+    """Represents a group of sets whose elements and provides the join operation. This is intended for constructing representations of elements that are related to each other if they are both related to another element. self[key] returns the set containing the key. Objects can also be deleted."""
+
     def __init__(self):
         self.internal_dict = {}
 
     def join(self, obj1: Hashable, obj2: Hashable) -> None:
+        """Sets in the group can be joined together by calling join on one element from each. If an object is joined into a set but is not already part of another set in the group, it is simply added to that set. If both objects that are joined together are not in an existing set, they form a new set together. """
         if obj1 in self.internal_dict:
             if obj2 in self.internal_dict:
                 self.__join_sets(obj1, obj2)
@@ -17,21 +20,26 @@ class SetGroup:
             else:
                 self.__new_set(obj1, obj2)
 
+    def __len__(self):
+        return len(self.internal_dict)
+
     def __getitem__(self, obj: Hashable) -> set:
         return self.internal_dict[obj]
+
+    def __delitem__(self, obj: Hashable):
+        self.internal_dict[obj].remove(obj)
+        del self.internal_dict[obj]
 
     def __contains__(self, obj: Hashable) -> bool:
         return obj in self.internal_dict
 
-    def get_all(self) -> list[set]:
+    def __iter__(self):
         found = set()
-        sets = []
         for key in self.internal_dict:
             if key in found:
                 continue
             found |= self.internal_dict[key]
-            sets.append(self.internal_dict[key])
-        return sets
+            yield self.internal_dict[key]
 
     def __new_set(self, obj1: Hashable, obj2: Hashable) -> None:
         self.internal_dict[obj1] = {obj1, obj2}
