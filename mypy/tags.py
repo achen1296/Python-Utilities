5f9e73a1aa74c85dde75cd6c8dc3ab86f5f0bfc4
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-08-16 18:12:45 -0400

    fix set method conflict with builtin set type

--- mypy/tags.py
+++ mypy/tags.py
@@ -1,5 +1,5 @@
 import os
-
+from builtins import set as bset
 from mypy import files
 
 
@@ -18,33 +18,18 @@ def name_and_ext(filename: str) -> tuple[str, str]:
     return (name, ext)
 
 
-def add(filename: str, new_tags: set[str]) -> str:
+def add(filename: str, new_tags: bset[str]) -> str:
     return set(filename, get(filename) | new_tags)
 
 
-def remove(filename: str, remove_tags: set[str]) -> str:
+def remove(filename: str, remove_tags: bset[str]) -> str:
     return set(filename, get(filename) - remove_tags)
 
 
-def set(filename: str, tags: set[str]) -> str:
-    name, ext = name_and_ext(filename)
-    if len(tags) == 0:
-        return name + ext
-    tags_str = ""
-    first = True
-    for tag in sorted(tags):
-        if first:
-            first = False
-        else:
-            tags_str += " "
-        tags_str += tag
-    return name + "[" + tags_str + "]" + ext
-
-
-def get(filename: str) -> set[str]:
+def get(filename: str) -> bset[str]:
     lbr = filename.rfind("[")
     rbr = filename.rfind("]")
-    tags = set()
+    tags = bset()
     if rbr != -1 and lbr != -1 and lbr < rbr:
         for tag in filename[lbr + 1:rbr].split(" "):
             tag = tag.strip()
@@ -59,7 +44,7 @@ def set_name(filename: str, new_name: str) -> str:
     return new_name + filename[len(name):]
 
 
-def tag_all_in(root: os.PathLike, new_tags: set[str], visit_subdirs: bool = True) -> None:
+def tag_all_in(root: os.PathLike, new_tags: bset[str], visit_subdirs: bool = True) -> None:
     planned_moves = {}
 
     for dirpath, dirnames, filenames in os.walk(root):
@@ -74,21 +59,51 @@ def tag_all_in(root: os.PathLike, new_tags: set[str], visit_subdirs: bool = True
     files.move_by_dict(planned_moves)
 
 
-def collect(root: os.PathLike) -> set[str]:
-    collected_tags = set()
+def untag_all_in(root: os.PathLike, remove_tags: bset[str], visit_subdirs: bool = True) -> None:
+    planned_moves = {}
+
+    for dirpath, dirnames, filenames in os.walk(root):
+        for f in filenames:
+            new_name = remove(f, remove_tags)
+            if f != new_name:
+                planned_moves[dirpath + os.sep +
+                              f] = dirpath + os.sep + new_name
+        if not visit_subdirs:
+            break
+
+    files.move_by_dict(planned_moves)
+
+
+def collect(root: os.PathLike) -> bset[str]:
+    collected_tags = bset()
     for dirpath, dirnames, filenames in os.walk(root):
         for f in filenames:
             collected_tags |= get(f)
     return collected_tags
 
 
-def map_to_folders(root: os.PathLike, tags: set[str]) -> dict[str, set[os.PathLike]]:
+def map_to_folders(root: os.PathLike, tags: bset[str]) -> dict[str, bset[os.PathLike]]:
     tags_to_folders = {}
     for dirpath, dirnames, filenames in os.walk(root):
         for d in dirnames:
             for t in tags:
                 if t in d:
                     if t not in tags_to_folders:
-                        tags_to_folders[t] = set()
+                        tags_to_folders[t] = bset()
                     tags_to_folders[t].add(d)
     return tags_to_folders
+
+
+def set(filename: str, tags: bset[str]) -> str:
+    name, ext = name_and_ext(filename)
+    if len(tags) == 0:
+        return name + ext
+    tags_str = ""
+    first = True
+    for tag in sorted(tags):
+        if first:
+            first = False
+        else:
+            tags_str += " "
+        tags_str += tag
+    return name + "[" + tags_str + "]" + ext
