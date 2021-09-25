56ef83b15d4c23ea175feeaf5d657470aa1c0a1b
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-09-25 18:35:05 -0400

    improved comments and type annotations

--- mypy/dictionaries.py
+++ mypy/dictionaries.py
@@ -7,7 +7,14 @@ from mypy import files
 
 
 def read_iterable_dict(list_dict: Iterable[str], *, key_value_separator: str = "\s*>\s*", value_list_separator: str = "\s*\|\s*",  comment: str = "\s*#", all_lists: bool = False) -> dict[str, Union[str, list[str]]]:
-    """ Reads a dictionary from a list of string entries, by default each is interpreted as key>value or key>val1|val2|... ">" is the default key/value separator and "|" is the default value list separator so that file paths can be keys/values. The default comment starter is "#" and entries that are one element long are not turned into lists by default. Use "" for no comments or no value list separator. All string parameters are used as regular expressions. """
+    """ Reads a dictionary from a list of string entries.
+
+    By default each is interpreted as key>value or key>val1|val2|... 
+    If the k/v separator is not present, the value is automatically filled in with "" (or [] for all_lists=True), and if present multiple times in an entry, those after the first are interpreted as value text.
+
+    ">" is the default key/value separator and "|" is the default value list separator so that file paths can be keys/values. The default comment starter is "#". Use "" for no comments or no value list separator. All string parameters are used as regular expressions.
+
+    Entries that are one element long are not turned into lists by default. """
     d = {}
     for entry in list_dict:
         # skip whitespace and comments
@@ -31,19 +38,23 @@ def read_iterable_dict(list_dict: Iterable[str], *, key_value_separator: str = "
 
 
 def read_string_dict(string_dict: str, *, entry_separator="\s*\n\s*", **kwargs) -> dict[str, Union[str, list[str]]]:
-    """ Reads a dictionary from a string, splitting on entry_separator and then using read_iterable_dict. All string parameters except the string to read are used as regular expressions. """
+    """ Reads a dictionary from a string, splitting on entry_separator and then using read_iterable_dict (passing kwargs). All string parameters except the string to read are used as regular expressions. """
     return read_iterable_dict(re.split(entry_separator, string_dict), **kwargs)
 
 
 def read_file_dict(filename: os.PathLike, *, encoding=None, entry_separator="\s*\n\s*", **kwargs) -> dict[str, Union[str, list[str]]]:
-    """ Reads a dictionary from a file, converting it to a string and using read_string_dict. All string parameters except filename are used as regular expressions. """
+    """ Reads a dictionary from a file (using the specified encoding), converting it to a string and using read_iterable_dict (passing kwargs). All string parameters except filename and encoding are used as regular expressions. """
     if filename == None:
         return {}
     return read_iterable_dict(files.re_split(filename, entry_separator, encoding=encoding), **kwargs)
 
 
 def write_iterable_dict(dictionary: dict[typing.Any, Union[typing.Any, Iterable[typing.Any]]], *, key_value_separator: str = ">", value_list_separator: str = "|") -> list[str]:
-    """Default separators > and |, same as reading dictionaries. """
+    """ Default separators > and |, same as reading dictionaries. 
+
+    Keys and values can be of any type and are converted to strings using str(). For iterable values, they are converted one at a time with the separator inserted. 
+
+    Keys with None or empty string values do not get the k/v separator. """
     l = []
     for key in sorted(dictionary):
         val = dictionary[key]
@@ -69,19 +80,22 @@ def write_iterable_dict(dictionary: dict[typing.Any, Union[typing.Any, Iterable[
     return l
 
 
-def write_string_dict(dictionary: dict[str, Union[str, Iterable[str]]], *, entry_separator: str = "\n", **kwargs) -> str:
+def write_string_dict(dictionary: dict[typing.Any, Union[typing.Any, Iterable[typing.Any]]], *, entry_separator: str = "\n", **kwargs) -> str:
+    """ Joins the results of write_iterable_dict (passing kwargs) with the specified entry separator. """
     return entry_separator.join(write_iterable_dict(dictionary, **kwargs))
 
 
-def write_file_dict(filename: os.PathLike, dictionary: dict[str, Union[str, list[str]]], **kwargs) -> None:
+def write_file_dict(filename: os.PathLike, dictionary: dict[typing.Any, Union[typing.Any, Iterable[typing.Any]]], **kwargs) -> None:
+    """ Writes the result of write_string_dict to a file. """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(write_string_dict(dictionary, **kwargs))
 
 
 def flip_dict(d: dict) -> dict:
+    """ Produces a new dictionary where each original key becomes the value of its value (if just one value) or each of its original values (an iterable non-string value). """
     new_d = {}
     for key, val in d.items():
-        if isinstance(val, Iterable):
+        if isinstance(val, Iterable) and not isinstance(val, str):
             for v in val:
                 new_d[v] = key
         else:
