941eb8557f8841e949a37cd722a66d29954b2ad7
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-09-25 19:59:59 -0400

    refactor to use re, pathlib, use _ for throwaways, and add tests

--- mypy/tags.py
+++ mypy/tags.py
@@ -3,20 +3,19 @@ import os
 from builtins import set as bset
 from mypy import files
 from pathlib import Path
+import re
+
+
+class TagException(Exception):
+    pass
 
 
 def name_and_ext(filename: str) -> tuple[str, str]:
-    lbr = filename.rfind("[")
-    rbr = filename.rfind("]")
-    if rbr != -1 and lbr != -1 and lbr < rbr:
-        # existing tags
-        name = filename[:lbr]
-        ext = filename[rbr + 1:]
-    else:
-        # no existing tags
-        dot = filename.rfind(".")
-        name = filename[:dot]
-        ext = filename[dot:]
+    match = re.match("(.*?)(\[.*\])?(\..*)", filename)
+    if not match:
+        raise TagException("Filename was not of correct format")
+    name = match.group(1)
+    ext = match.group(3)
     return (name, ext)
 
 
@@ -29,50 +28,36 @@ def remove(filename: str, remove_tags: typing.Iterable[str]) -> str:
 
 
 def get(filename: str) -> bset[str]:
-    lbr = filename.rfind("[")
-    rbr = filename.rfind("]")
-    tags = bset()
-    if rbr != -1 and lbr != -1 and lbr < rbr:
-        for tag in filename[lbr + 1:rbr].split(" "):
-            tag = tag.strip()
-            if tag == "":
-                continue
-            tags.add(tag.lower())
-    return tags
+    match = re.match("(.*?)(\[.*\])?\.(.*)", filename)
+    if not match:
+        raise TagException("Filename was not in correct format")
+    tags = match.group(2)
+    if tags == None:
+        return bset()
+    else:
+        # remove []
+        tags = tags[1:-1]
+        tags = re.split("\s+", tags)
+        return bset(tags)
 
 
 def set_name(filename: str, new_name: str) -> str:
-    name, ext = name_and_ext(filename)
+    name, _ = name_and_ext(filename)
     return new_name + filename[len(name):]
 
 
-def tag_all_in(root: os.PathLike, new_tags: typing.Iterable[str], visit_subdirs: bool = True) -> None:
-    new_tags = bset(new_tags)
+def tag_all_in(root: os.PathLike, tags: typing.Iterable[str], *, visit_subdirs: bool = True, remove_tags=False) -> None:
+    tags = bset(tags)
     planned_moves = {}
 
-    for dirpath, dirnames, filenames in os.walk(root):
+    for dirpath, _, filenames in os.walk(root):
         for f in filenames:
-            new_name = add(f, new_tags)
+            if remove_tags:
+                new_name = remove(f, tags)
+            else:
+                new_name = add(f, tags)
             if f != new_name:
-                planned_moves[dirpath + os.sep +
-                              f] = dirpath + os.sep + new_name
-        if not visit_subdirs:
-            break
-
-    files.move_by_dict(planned_moves)
-
-
-def untag_all_in(root: os.PathLike, remove_tags: typing.Iterable[str], visit_subdirs: bool = True) -> None:
-    remove_tags = bset(remove_tags)
-
-    planned_moves = {}
-
-    for dirpath, dirnames, filenames in os.walk(root):
-        for f in filenames:
-            new_name = remove(f, remove_tags)
-            if f != new_name:
-                planned_moves[dirpath + os.sep +
-                              f] = dirpath + os.sep + new_name
+                planned_moves[Path(dirpath, f)] = Path(dirpath, new_name)
         if not visit_subdirs:
             break
 
@@ -81,7 +66,7 @@ def untag_all_in(root: os.PathLike, remove_tags: typing.Iterable[str], visit_sub
 
 def collect(root: os.PathLike) -> bset[str]:
     collected_tags = bset()
-    for dirpath, dirnames, filenames in os.walk(root):
+    for _, _, filenames in os.walk(root):
         for f in filenames:
             collected_tags |= get(f)
     return collected_tags
@@ -89,7 +74,7 @@ def collect(root: os.PathLike) -> bset[str]:
 
 def map_to_folders(root: os.PathLike, tags: typing.Iterable[str]) -> dict[str, bset[os.PathLike]]:
     tags_to_folders = {}
-    for dirpath, dirnames, filenames in os.walk(root):
+    for dirpath, dirnames, _ in os.walk(root):
         for d in dirnames:
             for t in tags:
                 if t in d:
@@ -106,12 +91,36 @@ def set(filename: str, tags: typing.Iterable[str]) -> str:
     name, ext = name_and_ext(filename)
     if len(tags) == 0:
         return name + ext
-    tags_str = ""
-    first = True
-    for tag in sorted(tags):
-        if first:
-            first = False
-        else:
-            tags_str += " "
-        tags_str += tag
-    return name + "[" + tags_str + "]" + ext
+    return name + "[" + " ".join([t.strip() for t in sorted(tags)]) + "]" + ext
+
+
+if __name__ == "__main__":
+    result = name_and_ext("file.txt")
+    assert result == ("file", ".txt"), result
+    result = name_and_ext("file[tag1].txt")
+    assert result == ("file", ".txt"), result
+    result = name_and_ext("file[tag1 tag2].txt")
+    assert result == ("file", ".txt"), result
+    try:
+        name_and_ext("asdf")
+        assert False, "expected this to fail"
+    except TagException:
+        pass
+
+    result = get("file.txt")
+    assert result == bset(), result
+    result = get("file[tag1].txt")
+    assert result == {"tag1"}, result
+    result = get("file[tag1      tag2].txt")
+    assert result == {"tag1", "tag2"}, result
+
+    result = add("file.txt", ["tag1", "tag2"])
+    assert result == "file[tag1 tag2].txt", result
+    result = add("file[tag3].txt", ["tag1              ", "tag2"])
+    assert result == "file[tag1 tag2 tag3].txt", result
+    result = remove("file.txt", ["tag1", "tag2"])
+    assert result == "file.txt", result
+    result = remove("file[tag1 tag2].txt", ["tag1", "tag2"])
+    assert result == "file.txt", result
+    result = remove("file[tag3].txt", ["tag1", "tag2"])
+    assert result == "file[tag3].txt", result
