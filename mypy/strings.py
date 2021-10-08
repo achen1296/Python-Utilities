dd11f3fe353dc2d2b9f31925265e49ef8a629d71
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-10-07 22:50:53 -0400

    implement new find_pairs using next_match

--- mypy/strings.py
+++ mypy/strings.py
@@ -137,22 +137,27 @@ def last_match(regular_expressions: typing.Iterable[str], s: str, *, no_overlap=
             yield(last_r, last_match)
 
 
+def matches_any(regular_expressions: typing.Iterable[str], s: str, *, flags=0):
+    for r in regular_expressions:
+        if re.match(r, s, flags):
+            return True
+    return False
+
+
 class NoPairException(Exception):
     pass
 
 
-def find_pairs(s: str, start: int = 0, *, pairs: dict[str, str] = None, ignore_internal_pairs=None) -> list[tuple[int, int]]:
-    """ Finds pairs in the string of the specified characters and returns start/end indices.
+def find_pairs(s: str, *, pairs: dict[str, str] = None, ignore_internal_pairs: typing.Iterable[str] = None) -> list[tuple[int, int]]:
+    """ Finds pairs in the string of the specified expressions and returns the indices of the start and end of each pair (the first index of the matches).
 
-    Raises NoPairException if a pair is not found.
+    Any unbalanced pairs result in a NoPairException. The expressions for pair starts and ends should not allow overlap with themselves (although pair starts and ends can be the same character, like quotes), otherwise the results are undefined.
 
     For example, `find_pairs("[ex(am)\\"{\\"p\\\\]le]")`:
 
-    First, the [ is found. Then ( is found, before matching immediately with ). Then the quote is found. Since it is inside quotes, the { is ignored. The next matching quote is found. The next escaped \] is ignored, and finally the first [ is paired. Thus the complete return value is `[(0,15),(8,9),(3,6)]`, with the pairs noted down in the opposite order they are completed. 
+    First, the [ is found. Then ( is found, before matching immediately with ). Then the quote is found. Since it is inside quotes, the { is ignored. The next matching quote is found. The next escaped \] is ignored, and finally the first [ is paired. Thus the complete return value is `[(0,15),(7,9),(3,6)]` -- in reverse order of the pair end index.
 
-    If the `pairs` and `ignore_internal_pairs` parameters are left as None, this is the behavior. However, `pairs` can be specified as a dictionary, with the regular expression for a pair start mapping to the regular expression for the corresponding paired character. The ignoring of escaped characters is part of the default regular expressions for `pairs`. `ignore_internal_pairs` is a set of pair starter regular expressions and is only used if `pairs` matches a piece of the string first. 
-
-    Any unbalanced pairs result in a NoPairException. """
+    If the `pairs` and `ignore_internal_pairs` parameters are left as None, unescaped {, [, (, ", and ' characters are matched with their opposites. However, `pairs` can be specified as a dictionary, with the regular expression for a pair start mapping to the regular expression for the corresponding paired character. The ignoring of escaped characters (those preceded by an \\ character) is part of the default regular expressions for `pairs`. `ignore_internal_pairs` is a set of pair starter regular expressions and is only used if `pairs` matches a piece of the string first. """
 
     # negative look behind for \
     NO_ESCAPE = "(?<!\\\\)"
@@ -163,52 +168,89 @@ def find_pairs(s: str, start: int = 0, *, pairs: dict[str, str] = None, ignore_i
     if ignore_internal_pairs == None:
         ignore_internal_pairs = {"\"", "\'"}
 
-    start_str = None
-    for re_str in pairs:
-        re_comp = re.compile(re_str)
-        match = re_comp.match(s, start)
-        if match != None:
-            start_str = s[match.start():match.end()]
-            start_re = re_str
-            break
-    if start_str == None:
-        raise NoPairException(
-            f"No pair start at index {start} found in <{s}>")
-    end_re = re.compile(pairs[start_re])
+    ignore_internal_pairs = set(ignore_internal_pairs)
 
-    ignore_internal = False
-    for re_str in ignore_internal_pairs:
-        if re.match(re_str, start_str) != None:
-            ignore_internal = True
-            break
+    pair_starts_gen = next_match(pairs, s)
+    pair_ends_gen = next_match(pairs.values(), s)
 
-    i = start+1
-    length = len(s)
-    while i < length:
-        end_re = re.compile(end_re)
-        if end_re.match(s, i):
-            # print(f"{start}-{i}")
-            return i
-        if ignore_internal:
-            # simply continue if internal pairs are not being considered
-            i += 1
-        else:
-            # skip internal pairs
-            try:
-                i = find_pairs(s, i, pairs=pairs)+1
+    # return value, collected index pairs
+    pair_list = []
+    # stack of pair starts
+    start_stack = []
+    next_start = next(pair_starts_gen, None)
+    ignoring_internal = False
+    while True:
+        # get the next pair end
+        try:
+            next_end = next(pair_ends_gen)
+        except StopIteration:
+            # exhausted all pair ends
+            break
+        # push all starts before that onto the stack, which may be none (and should be sometimes)
+        while next_start != None and next_end != None and next_start[1].end() <= next_end[1].start():
+            if not ignoring_internal:
+                start_stack.append(next_start)
+            if matches_any(ignore_internal_pairs, next_start[1].group()):
+                # the rest of the starts up to the next end are consumed without being added to the stack
+                ignoring_internal = True
+            next_start = next(pair_starts_gen, None)
+        # make sure the last start pushed matches
+        try:
+            popped_start = start_stack.pop()
+        except IndexError:
+            # pair match failed; attempt to interpret next end as a start instead (e.g. quotes by default)
+            if next_start[1].span() == next_end[1].span():
                 continue
-            except NoPairException:
-                i += 1
-
-    raise ValueError(
-        f"Pair not found for {start_str} at index {start} in <{s}>")
+            raise NoPairException(
+                f"Ran out of pair starts to match with {next_end[1]}. {pair_list} {start_stack}")
+        if pairs[popped_start[0]] != next_end[0]:
+            # pair match failed; attempt to interpret next end as a start instead (e.g. quotes by default)
+            if next_start[1].span() == next_end[1].span():
+                # return popped pair start to stack
+                start_stack.append(popped_start)
+                continue
+            # not also a start
+            raise NoPairException(
+                f"Pair starting {popped_start[1]} did not match with a {pairs[popped_start[0]]}, {next_end[1]} found instead. {pair_list} {start_stack}")
+        # pair matched
+        pair_list = [(popped_start[1].start(),
+                      next_end[1].start())] + pair_list
+        ignoring_internal = False
+        # if end was also counted as a start, skip it
+        if next_start != None and next_start[1].span() == next_end[1].span():
+            next_start = next(pair_starts_gen, None)
+    # did not also exhaust all pair starts
+    if next_start != None:
+        raise NoPairException(
+            f"Pair starting {next_start[1]} did not match with a {pairs[next_start[0]]}, ran out of pair ends instead. {pair_list} {start_stack}")
+    return pair_list
 
 
 if __name__ == "__main__":
-    assert argument_split("\"{asd[ pow ]f pootis}\" ([]   A ) {pow} \{\{\{ \"asdf\"\n") == [
-        "{asd[ pow ]f pootis}", "([]   A )", "{pow}", "{{{", "asdf"]
-    result = find_pairs("[ex(am)\"{\"p\\]le]")
-    assert result == [(0, 15), (8, 9), (3, 6)], result
-    assert argument_split(
-        "type=minecraft:area_effect_cloud,tag=marker,limit=1,name=\\\"<marker name>\\\"", sep=",") == ["type=minecraft:area_effect_cloud", "tag=marker", "limit=1", "name=\"<marker name>\""]
-    assert unescape("\'a\\\\sdf\'") == "\'a\\sdf\'"
+    result = [m[1].span() for m in next_match(
+        ["[ab]*", "[bc]*"], "abcbc", no_overlap=False)]
+    assert result == [(0, 2), (1, 5), (3, 4)], result
+    result = [m[1].span() for m in next_match(
+        ["[ab]*", "[bc]*"], "abcbc", no_overlap=True)]
+    assert result == [(0, 2), (3, 4)], result
+
+    result = [m[1].span() for m in last_match(
+        ["[ab]*", "[bc]*"], "abcbc", no_overlap=False)]
+    assert result == [(1, 5), (3, 4), (0, 2)], result
+    result = [m[1].span() for m in last_match(
+        ["[ab]*", "[bc]*"], "abcbc", no_overlap=True)]
+    assert result == [(1, 5)], result
+    result = find_pairs("abcdefghi")
+    assert result == [], result
+    result = find_pairs("csdf(asdf)bsdf")
+    assert result == [(4, 9)], result
+    result = find_pairs("c(a[])")
+    assert result == [(1, 5), (3, 4)], result
+    result = find_pairs("c(a[]{})")
+    assert result == [(1, 7), (5, 6), (3, 4)], result
+    result = find_pairs("123\"asdf\"")
+    assert result == [(3, 8)], result
+    result = find_pairs("123\"a{{f\"")
+    assert result == [(3, 8)], result
+    result = find_pairs("(\"a{{f\"absd) {()  []}")
+    assert result == [(13, 20), (18, 19), (14, 15), (0, 11), (1, 6)], result
