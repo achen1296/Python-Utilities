4625c2b95d908ac872090903b605f0cf16c473e1
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2021-11-01 22:57:24 -0400

    more imports

--- python_startup.py
+++ python_startup.py
@@ -1,4 +1,11 @@
 import os
+import random
 import re
+import shutil
 import time
 from pathlib import Path
+
+import selenium
+
+from mypy import (dictionaries, files, ftp, images, lists, sets, strings, tags,
+                  web)
