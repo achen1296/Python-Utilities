23dc938f7b37eaea76178e57326350d685ed7d04
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2022-02-01 11:46:10 -0500

    import math + organize

--- python_startup.py
+++ python_startup.py
@@ -1,14 +1,14 @@
+import math
 import os
 import random
 import re
 import shutil
 import time
+from importlib import reload
 from pathlib import Path
-from PIL import Image
 
 import selenium
+from PIL import Image
 
 from mypy import (dictionaries, files, ftp, images, lists, sets, strings, tags,
                   web)
-
-from importlib import reload
