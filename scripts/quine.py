987caf08b39407ecc3a0c441cc8f0b2dcdc201cd
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2022-10-13 16:11:14 -0400

    move scripts elsewhere

--- scripts/quine.py
+++
@@ -1,6 +0,0 @@
-quotes = "\"\"\""
-code = r"""quotes = "\"\"\""
-code = r
-print(code[:26] + quotes + code + quotes + code[26:], end="")
-"""
-print(code[:26] + quotes + code + quotes + code[26:], end="")
