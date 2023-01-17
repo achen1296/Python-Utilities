f170f920c1ef1fcf67d4aa3f1873eb3eddc0b5d1
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2023-01-17 17:42:56 -0500

    import stale element exception

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ import typing
 import requests
 from selenium import webdriver
 from selenium.common.exceptions import (NoSuchElementException,
-                                        NoSuchWindowException)
+                                        NoSuchWindowException, StaleElementReferenceException)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
