f5cb2282dcc2a751bb7a81b391e6816524d030f2
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2023-04-01 21:48:30 -0400

    dots tweaks

--- console.py
+++ console.py
@@ -8,33 +8,29 @@ from typing import Any, Callable, Iterable, Union
 class Dots:
     """ For printing dots to show that the console is working. Every time dot() is called, a counter increments and the time since the last dot was printed is evaluated. If there were both enough calls and enough time to exceed both the minimum count and the minimum time (in seconds), a dot is printed. """
 
-    def __init__(self, min_count: int, min_time: float, output_callback=None, output_str="."):
+    def __init__(self, min_count: int = 100, min_time: float = 1.0, output_str="."):
         self._count = 0
         self._last_time = time.monotonic()
         self._min_count = min_count
         self._min_time = min_time
-        if output_callback is None:
-            self._output_callback = lambda s: print(s, end="", flush=True)
-        else:
-            self._output_callback = output_callback
         self._output_str = output_str
 
     def dot(self):
         self._count += 1
         if self._count >= self._min_count and time.monotonic() - self._last_time >= self._min_time:
             self.reset()
-            self._output_callback(self._output_str)
+            print(self._output_str, end="", flush=True)
 
     def reset(self):
         self._count = 0
         self._last_time = time.monotonic()
 
 
-DOTS = Dots(100, 1.0)
+DOTS = Dots()
 
 
 def dot():
-    """ Calls dot on shared Dots(100, 1.0) object DOTS """
+    """ Calls dot on shared Dots object DOTS """
     DOTS.dot()
 
 
