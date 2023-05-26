891336f50938b218573daf7ab163e582ae954b45
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2023-05-26 09:27:29 -0400

    analogous functions to wait_element that also get attribute, click, or send keys

--- web.py
+++ web.py
@@ -1,12 +1,13 @@
 import os
 import time
-from pathlib import Path
-from typing import Callable, Iterable
+from typing import Iterable, Union
 from urllib.parse import urlparse
 
 import requests
 from selenium import webdriver
-from selenium.common.exceptions import (NoSuchElementException,
+from selenium.common.exceptions import (ElementClickInterceptedException,
+                                        ElementNotInteractableException,
+                                        NoSuchElementException,
                                         NoSuchWindowException,
                                         StaleElementReferenceException)
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -156,6 +157,49 @@ def wait_element(driver: WebDriver, css_selector: str, timeout: int = 10) -> Web
 def wait_url(driver: WebDriver, url_contains: str, timeout: int = 10):
     return WebDriverWait(driver, timeout).until(
         expected_conditions.url_contains(url=url_contains))
+
+
+def find_and_get_attribute(driver: WebDriver, css_selector: str, attribute: str, *, all: bool = False, timeout: int = 10, index: int = 0, ignored_exceptions: Iterable[Exception] = (ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException)) -> Union[str, Iterable[str]]:
+    """ If all=True then will return a list of the results for all elements found, or specify an index > 0 to select only that index out of the list of all matching elements. """
+    def try_get_attribute(driver: WebDriver):
+        if all:
+            es = driver.find_elements(By.CSS_SELECTOR, css_selector)
+            return [e.get_attribute(attribute) for e in es]
+        if index == 0:
+            e = driver.find_element(By.CSS_SELECTOR, css_selector)
+            return e.get_attribute(attribute)
+        es = driver.find_elements(By.CSS_SELECTOR, css_selector)
+        return es[index].get_attribute(attribute)
+
+    return WebDriverWait(driver, timeout, ignored_exceptions=ignored_exceptions).until(try_get_attribute)
+
+
+def find_and_click(driver: WebDriver, css_selector: str, *, timeout: int = 10,  index: int = 0, ignored_exceptions: Iterable[Exception] = (ElementClickInterceptedException, ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException)):
+    def try_click(driver: WebDriver):
+        if index == 0:
+            e = driver.find_element(By.CSS_SELECTOR, css_selector)
+            e.click()
+            return True
+        es = driver.find_elements(By.CSS_SELECTOR, css_selector)
+        es[index].click()
+        return True
+
+    WebDriverWait(driver, timeout,
+                  ignored_exceptions=ignored_exceptions).until(try_click)
+
+
+def find_and_send_keys(driver: WebDriver, css_selector: str, *keys, timeout: int = 10, index: int = 0,  ignored_exceptions: Iterable[Exception] = (ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException)):
+    def try_send_keys(driver: WebDriver):
+        if index == 0:
+            e = driver.find_element(By.CSS_SELECTOR, css_selector)
+            e.send_keys(*keys)
+            return True
+        es = driver.find_elements(By.CSS_SELECTOR, css_selector)
+        es[index].send_keys(*keys)
+        return True
+
+    WebDriverWait(driver, timeout, ignored_exceptions=ignored_exceptions).until(
+        try_send_keys)
 
 
 def scroll_all_elements(driver: WebDriver, css_selector: str, *, interactable_selector: str = None, last_selector: str = None, wait=1) -> list[WebElement]:
