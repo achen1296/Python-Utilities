16a6181a449a0a452a6c030465f6d697a4eaa69e
vcpaewbn <69118040+vcpaewbn@users.noreply.github.com>
2024-09-09 15:44:08 -0400

    add flush (in case close does something else in the future)

--- file_backed_data.py
+++ file_backed_data.py
@@ -7,7 +7,7 @@ class FileBackedData:
     """ Subclasses should only need to override read() and write(). """
 
     def __init__(self, file: files.PathLike, read_args: tuple = (), read_kwargs: dict = {}, write_args: tuple = (), write_kwargs: dict = {}):
-        """ Use as a context manager or explicitly call close(). Either way, will use the write_args and write_kwargs when calling write(). """
+        """ Use as a context manager or explicitly call flush or close(). Either way, will use the write_args and write_kwargs when calling write(). """
         self.file = Path(file)
 
         self.read(*read_args, **read_kwargs)
@@ -22,6 +22,10 @@ class FileBackedData:
         pass
 
     def close(self):
+        # currently just flushes, but might do something different in the future...
+        self.flush()
+
+    def flush(self):
         self.write(*self.write_args, **self.write_kwargs)
 
     def __enter__(self): return self
