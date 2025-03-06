367c33832a081a2041fbad6c91b377617e182d59
achen1296 <achen1296@gmail.com>
2025-03-06 09:57:26 -0500

    int input utils

---
+++ input_utils.py
@@ -0,0 +1,22 @@
+def get_int_input(prompt: str = "", err_prompt: str = "Please input a valid integer"):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print(err_prompt)
+
+
+def get_int_input_in_range(range: int | tuple[int, int], prompt: str = "", err_prompt: str | None = None):
+    if isinstance(range, int):
+        min = 0
+        max = range
+    else:
+        min, max = range
+    if err_prompt is None:
+        err_prompt = f"Please input a valid integer in the range {min}-{max}"
+    while True:
+        i = get_int_input(prompt, err_prompt)
+        if min <= i <= max:
+            return i
+        else:
+            print(err_prompt)
