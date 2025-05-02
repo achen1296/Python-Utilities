39e2d190da91062dfaf95205425e2b63d93ed86a
achen1296 <achen1296@gmail.com>
2025-05-02 16:39:50 -0400

    separate code that requires numpy so as not to cause errors if it is not present, as much of the lists module can be used without it

--- lists.py
+++ lists.py
@@ -4,11 +4,7 @@ import math
 import random
 import re
 from io import StringIO
-from pathlib import Path
-from typing import (Any, Callable, Hashable, Iterable, Literal, Self, Sequence, Sized,
-                    SupportsIndex)
-
-import numpy as np
+from typing import (Any, Callable, Hashable, Iterable, Literal, Sequence)
 
 import files
 from file_backed_data import FileBackedData
@@ -152,126 +148,131 @@ class FileBackedList[T](FileBackedData, list):
                         **kwargs)
 
 
-class TriangularArray[T]:
-    """ Represents a square 2D array that is symmetrical, i.e. where swapping the indices points to the same information, without actually duplicating values. Can be `expand`ed but not contracted (copy the values to a new, smaller one instead if desired).
+try:
+    import numpy as np
+except ModuleNotFoundError:
+    pass
+else:
+    class TriangularArray[T]:
+        """ Represents a square 2D array that is symmetrical, i.e. where swapping the indices points to the same information, without actually duplicating values. Can be `expand`ed but not contracted (copy the values to a new, smaller one instead if desired).
 
-    Note that it behaves differently from an actual 2D array in that:
-    - `len` will return the side length, not the number of elements
-    - `iter` will return an iterator that produces `r`(ow index), `c`(olumn index), `v`(alue) tuples, not rows
-    """
+        Note that it behaves differently from an actual 2D array in that:
+        - `len` will return the side length, not the number of elements
+        - `iter` will return an iterator that produces `r`(ow index), `c`(olumn index), `v`(alue) tuples, not rows
+        """
 
-    @staticmethod
-    def triangular_number(n: int):
-        return n*(n+1)//2
+        @staticmethod
+        def triangular_number(n: int):
+            return n*(n+1)//2
 
-    @staticmethod
-    def _index(x: int, y: int):
-        small = min(x, y)
-        large = max(x, y)
-        return TriangularArray.triangular_number(large)+small
+        @staticmethod
+        def _index(x: int, y: int):
+            small = min(x, y)
+            large = max(x, y)
+            return TriangularArray.triangular_number(large)+small
 
-    def __init__(self, side_length: int | None = None, *, data: list[T] | np.ndarray | None = None, numpy_array=False, default_value: T = 0.):
-        """ Specify either side_length or data. data will take precedence if it is not None, and the correct side_length will be calculated accordingly. This will also override the numpy_array argument. data is expected to be in the same format/memory order this class creates, not an ordinary 2D array, so that the class can be easily copied/expanded/loaded from file. If the data length is not a triangular number, it is filled with default_value until it is. """
-        self.default_value = default_value
+        def __init__(self, side_length: int | None = None, *, data: list[T] | np.ndarray | None = None, numpy_array=False, default_value: T = 0.):
+            """ Specify either side_length or data. data will take precedence if it is not None, and the correct side_length will be calculated accordingly. This will also override the numpy_array argument. data is expected to be in the same format/memory order this class creates, not an ordinary 2D array, so that the class can be easily copied/expanded/loaded from file. If the data length is not a triangular number, it is filled with default_value until it is. """
+            self.default_value = default_value
 
-        self.data: list[T] | np.ndarray
-        self.side_length: int
-        self.numpy_array: bool
+            self.data: list[T] | np.ndarray
+            self.side_length: int
+            self.numpy_array: bool
 
-        if data is not None:
-            self.data = data
-            self.numpy_array = isinstance(self.data, np.ndarray)
-            l = len(data)
-            # check if length is a triangular number
-            # if l = n*(n+1)//2, n an integer, then n < sqrt(2*l) < n+1 -> floor(sqrt(2*l)) == n
-            n = math.floor(math.sqrt(2*l))
-            t = self.triangular_number(n)
-            if l > t:
-                # can have both l < t(n) (e.g. l=2 -> t(n)=3) and l > t (e.g. l=4 -> also t(n)=3)
-                # but always have t(n-1) < l < t(n+1), so either t(n) or t(n+1) is the next largest triangular number
-                # therefore if is sufficient, while is not needed
-                n += 1
+            if data is not None:
+                self.data = data
+                self.numpy_array = isinstance(self.data, np.ndarray)
+                l = len(data)
+                # check if length is a triangular number
+                # if l = n*(n+1)//2, n an integer, then n < sqrt(2*l) < n+1 -> floor(sqrt(2*l)) == n
+                n = math.floor(math.sqrt(2*l))
                 t = self.triangular_number(n)
-            self._resize(l, self.triangular_number(n))
-            self.side_length = n
-        else:
-            self.numpy_array = numpy_array
-            if side_length is None:
-                raise TypeError("must specify side_length if data is not specified")
-            self.side_length = side_length
-            one_d_length = self.triangular_number(side_length)
-            if numpy_array:
-                self.data = np.ndarray((one_d_length,))
-                self.data.fill(default_value)
+                if l > t:
+                    # can have both l < t(n) (e.g. l=2 -> t(n)=3) and l > t (e.g. l=4 -> also t(n)=3)
+                    # but always have t(n-1) < l < t(n+1), so either t(n) or t(n+1) is the next largest triangular number
+                    # therefore if is sufficient, while is not needed
+                    n += 1
+                    t = self.triangular_number(n)
+                self._resize(l, self.triangular_number(n))
+                self.side_length = n
             else:
-                self.data = [default_value] * one_d_length
+                self.numpy_array = numpy_array
+                if side_length is None:
+                    raise TypeError("must specify side_length if data is not specified")
+                self.side_length = side_length
+                one_d_length = self.triangular_number(side_length)
+                if numpy_array:
+                    self.data = np.ndarray((one_d_length,))
+                    self.data.fill(default_value)
+                else:
+                    self.data = [default_value] * one_d_length
 
-    class Row:
-        def __init__(self, tri_array: "TriangularArray[T]", x: int):
-            self.tri_array = tri_array
-            self.x = x
+        class Row:
+            def __init__(self, tri_array: "TriangularArray[T]", x: int):
+                self.tri_array = tri_array
+                self.x = x
 
-        def __getitem__(self, y: int) -> T:
-            return self.tri_array.data[self.tri_array._index(self.x, y)]
+            def __getitem__(self, y: int) -> T:
+                return self.tri_array.data[self.tri_array._index(self.x, y)]
 
-        def __setitem__(self, y: int, v: T):
-            self.tri_array.data[self.tri_array._index(self.x, y)] = v
+            def __setitem__(self, y: int, v: T):
+                self.tri_array.data[self.tri_array._index(self.x, y)] = v
 
-        def __iter__(self) -> Iterable[T]:
-            for c in range(0, self.tri_array.side_length):
-                yield self.tri_array.data[self.tri_array._index(self.x, c)]
+            def __iter__(self) -> Iterable[T]:
+                for c in range(0, self.tri_array.side_length):
+                    yield self.tri_array.data[self.tri_array._index(self.x, c)]
+
+            def __len__(self):
+                return len(self.tri_array)
+
+        def __getitem__(self, x: int):
+            return self.Row(self, x)
+
+        def __setitem__(self, x: int, values: Sequence[T]):
+            if len(values) != self.side_length:
+                raise TypeError("values must be same length as a single row")
+            for y in range(0, self.side_length):
+                self.data[self._index(x, y)] = values[y]
+
+        def __iter__(self) -> Iterable[tuple[int, int, T]]:
+            i = 0
+            for x in range(0, self.side_length):
+                for y in range(0, x+1):
+                    # assert self._index(x, y) == i
+                    yield x, y, self.data[i]
+                    i += 1
 
         def __len__(self):
-            return len(self.tri_array)
+            return self.side_length
 
-    def __getitem__(self, x: int):
-        return self.Row(self, x)
+        def __str__(self):
+            s = StringIO()
+            s.write("[\n")
+            i = 0
+            for x in range(0, self.side_length):
+                s.write("\t[")
+                for y in range(0, x+1):
+                    # assert self._index(x, y) == i
+                    s.write(f"{self.data[i]}, ")
+                    i += 1
+                s.write("]\n")
+            s.write("]")
+            return s.getvalue()
 
-    def __setitem__(self, x: int, values: Sequence[T]):
-        if len(values) != self.side_length:
-            raise TypeError("values must be same length as a single row")
-        for y in range(0, self.side_length):
-            self.data[self._index(x, y)] = values[y]
+        def __repr__(self):
+            return self.__str__()
 
-    def __iter__(self) -> Iterable[tuple[int, int, T]]:
-        i = 0
-        for x in range(0, self.side_length):
-            for y in range(0, x+1):
-                # assert self._index(x, y) == i
-                yield x, y, self.data[i]
-                i += 1
+        def expand(self, n: int):
+            old_length = self.triangular_number(self.side_length)
+            self.side_length += n
+            new_length = self.triangular_number(self.side_length)
+            self._resize(old_length, new_length)
 
-    def __len__(self):
-        return self.side_length
-
-    def __str__(self):
-        s = StringIO()
-        s.write("[\n")
-        i = 0
-        for x in range(0, self.side_length):
-            s.write("\t[")
-            for y in range(0, x+1):
-                # assert self._index(x, y) == i
-                s.write(f"{self.data[i]}, ")
-                i += 1
-            s.write("]\n")
-        s.write("]")
-        return s.getvalue()
-
-    def __repr__(self):
-        return self.__str__()
-
-    def expand(self, n: int):
-        old_length = self.triangular_number(self.side_length)
-        self.side_length += n
-        new_length = self.triangular_number(self.side_length)
-        self._resize(old_length, new_length)
-
-    def _resize(self, old_length: int, new_length: int):
-        if self.numpy_array:
-            assert isinstance(self.data, np.ndarray)
-            self.data.resize(new_length, refcheck=False)
-            self.data[old_length:].fill(self.default_value)
-        else:
-            assert isinstance(self.data, list)
-            self.data += [self.default_value] * (new_length-old_length)
+        def _resize(self, old_length: int, new_length: int):
+            if self.numpy_array:
+                assert isinstance(self.data, np.ndarray)
+                self.data.resize(new_length, refcheck=False)
+                self.data[old_length:].fill(self.default_value)
+            else:
+                assert isinstance(self.data, list)
+                self.data += [self.default_value] * (new_length-old_length)
