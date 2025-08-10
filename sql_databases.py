2bd2ef87b70ea9ef304cdfee4dcb59e3d17e7bd1
achen1296 <achen1296@gmail.com>
2025-08-10 16:17:23 -0400

    dropped using .import for CSVs because having to launch a separate process resulted in frequent database locking issues

--- sql_databases.py
+++ sql_databases.py
@@ -347,38 +347,15 @@ class Table:
 
     def import_csv(self, csv_file: Path | str, *, add_missing_columns: bool = False, ignore_extra_data=False, upsert=False):
         """ Cannot add types to columns this way, as CSV reader would of course always produce string values. Returns count of entries added. """
-        with open(csv_file, encoding="utf-8-sig") as f:  # encoding handles byte order mark
-            reader = csv.reader(f)
-            try:
-                csv_cols = next(reader)
-            except StopIteration:
-                # empty file
-                return 0
-            try:
-                first_row = next(reader)
-            except StopIteration:
-                return 0 # CSV has header only, no data
-            operation_cols, _ = self._parse_row({c: v for c, v in zip(csv_cols, first_row)}, add_missing_columns=add_missing_columns, add_column_types=False, ignore_extra_data=ignore_extra_data)  # update columns and get columns to operate on
-
         with self.con:
             self.cur.execute(""" drop table if exists csv_temp_table """)
-            while (err := os.system(f""" sqlite3 "{self.db.db_file}" ".import '{csv_file}' csv_temp_table --csv" """)) != 0:
-                print(f"CSV import returned error code {err}, retrying after 5 seconds")
-                time.sleep(5)
 
-            sql = f""" insert into \"{self.name}\" ({cols_joined_str(operation_cols)})
-                select {cols_joined_str(operation_cols)} from csv_temp_table where true """  # where true needed for upsert clause https://sqlite.org/lang_upsert.html 2.2
-            if upsert:
-                sql += f""" on conflict do update set ({cols_joined_str(operation_cols)}) = ({",".join(f"excluded.\"{c}\"" for c in operation_cols)}) """
-            else:
-                sql += "on conflict do nothing"
-
-            count: int = 0
-            try:
-                self.cur.execute(sql)
-                count = self.cur.execute("select count(*) from csv_temp_table").fetchone()[0]
-            finally:
-                self.cur.execute("drop table if exists csv_temp_table")
+            count = 0
+            with open(csv_file, newline="", encoding="utf-8-sig") as f:  # encoding handles byte order mark
+                reader = csv.DictReader(f)
+                for row in reader:
+                    self.insert(row, add_missing_columns=add_missing_columns, ignore_extra_data=ignore_extra_data, upsert=upsert, add_column_types=False)
+                    count += 1
 
         return count
 
